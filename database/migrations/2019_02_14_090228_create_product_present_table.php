ee0d00448bfa2f647a5fd1a5e4975ddf7184bc8d
Carlos Ferrer Hernandez <carlosferrerhernandez10@gmail.com>
2019-02-14 02:11:40 -0500

    Creacion de migraciones

---
+++ database/migrations/2019_02_14_090228_create_product_present_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductPresentTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_present', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->unsignedInteger('producto_id');
+            $table->foreign('producto_id')->references('id')->on('productos')->onDelete('cascade');
+
+            $table->unsignedInteger('presentacion_id');
+            $table->foreign('presentacion_id')->references('id')->on('presentaciones')->onDelete('cascade');
+
+            $table->integer('costo');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_present');
+    }
+}
